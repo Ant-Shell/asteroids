64d2d27c8bf01d4dca7994c5bc035051e1058116
Anthony Shellman <atshellman@gmail.com>
2025-05-17 17:21:47 -0600

    import shot class, create shots group

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from constants import *
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+from shot import Shot
 import sys
 
 def main():
@@ -13,18 +14,18 @@ def main():
   updatable = pygame.sprite.Group()
   drawable = pygame.sprite.Group()
   asteroids = pygame.sprite.Group()
+  shots = pygame.sprite.Group()
 
   Player.containers = (updatable, drawable)
   Asteroid.containers = (asteroids, updatable, drawable)
-  AsteroidField.containers = (updatable)
+  AsteroidField.containers = (updatable,)
+  Shot.containers = (shots, updatable, drawable)
 
   player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
   asteroid_field = AsteroidField()
 
   dt = 0
   print("Starting Asteroids!")
-  print(f"Screen width: {SCREEN_WIDTH}")
-  print(f"Screen height: {SCREEN_HEIGHT}")
 
   while True:
     for event in pygame.event.get():
